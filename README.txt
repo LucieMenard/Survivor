25f4c77c03f9ca96b6174e6fa5f366769735095a
Lucie Ménard <lulu.menard@gmail.com>
2018-05-21 15:56:31 +0200

    v 21 mai 16h

---
+++ README.txt
@@ -0,0 +1,14 @@
+# Ce fichier a pour but d'aider le lancement du jeu
+
+Ce jeu a été développé dans le cadre des cours d'initiation à un projet informatique de l'ENIB au deuxième semestre.
+La contrainte du jeu était une balle avec des rebonds.
+
+Pour lancer le jeu :
+Dezipper le dossier
+Ouvrir un terminal et l'aggrandir
+Rentrer dans le dossier
+Lancer le fichier "main.py" avec python 2.7
+Suivre les instructions
+Amuser vous bien !
+
+Lucie Ménard, S2P-C, l7menard@enib.fr
---
+++ survivor/Background.py
@@ -0,0 +1,69 @@
+#-*-coding:utf-8 -*-
+#ce module crée notre fond. Il contiendra une chaine de caractères qui representera notre fond
+import sys
+
+def create(filename):
+    #Création du fond
+    fond={ ' map ' : [], ' fondTableau ' : []}
+
+    #Ouverture du fichier
+    myfile =  open(filename, 'r' )
+
+    #Charger le contenu
+    chaine = myfile.read()   #ou myfile.readlines()
+
+    #Fermer le fichier
+    myfile.close()
+
+    #Initialisation des structures
+    fond['map'] = chaine
+    fond ['fondTableau'] = conversion (chaine)
+
+    return fond
+
+def conversion(chaine):
+    #Initialisation des variables locales
+    tableau = []
+    line = []
+
+    #Parcours de la chaine de caractère éléments par éléments pour la conversion
+    for i in chaine :
+        #if i == "_" :               # Plafond
+            #line.append(1)
+        #elif i == "H" :             # Mur gauche
+            #line.append(2)
+        if i == "*" :             # Sol ou plateforme
+            line.append(3)
+        elif i == " " :             # Case vide
+            line.append(0)
+        #elif i == "F" :             # Mur droit
+            #line.append(4)
+        elif i == "\n" :            # Retour à la ligne (LF)
+            tableau.append(line)    # On ajoute la ligne à tableau
+            line = []               # On reinitialise la ligne
+        elif i == "\r" :            # Sous Windows/Mac, on peut avoir des caractères CR, on les ignore
+            pass
+        else :              # Test d'erreur
+            print ord(i), ": Erreur dans le fichier fond2.txt : caractère non supporté "
+    return tableau
+
+def getChar(fond, x, y):
+    print int(round(y)),int(round(x))
+    print fond['map'][int(round(y))][int(round(x))]
+    return (fond['map'][int(round(y))][int(round(x))])
+def setChar(fond, x, y, caractere):
+    fond['map'][y][x]= caractere
+
+def getElement(fond, x, y):
+    return fond['fondTableau'][int(round(y)) - 1][int(round(x)) - 1]
+
+def show(fond) :
+    #couleur fond
+    sys.stdout.write("\033[40m")
+    #couleur white
+    sys.stdout.write("\033[37m")
+    print fond['map']
+
+###test###
+fond=create("fond2.txt")
+show(fond)
